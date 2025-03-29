698e8095e212a825966ccfcabddea719500ea888
ShadowIF <karolporeba1212@gmail.com>
2025-03-29 18:22:50 +0100

    Map added

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -5,6 +5,65 @@ import { GeocodingControl } from "@maptiler/geocoding-control/maptilersdk";
 import "@maptiler/geocoding-control/style.css";
 import "./Map.css";
 
+const localizations = [
+  {
+    business_name: "Olimp Galeria Kazimierz",
+    longitude: 19.95632845621571,
+    latitude: 50.05395204215157,
+    discount: 15,
+    color: "green",
+  },
+  {
+    business_name: "Pizzatopia",
+    longitude: 19.93379159497496,
+    latitude: 50.063074839342995,
+    discount: 30,
+    color: "red",
+  },
+  {
+    business_name: "Weselże się pizza",
+    longitude: 19.897852450259162,
+    latitude: 50.07909885198227,
+    discount: 20,
+    color: "blue",
+  },
+  {
+    business_name: "Ikea restauracja",
+    longitude: 19.896996442338256,
+    latitude: 50.09107978590599,
+    discount: 12,
+    color: "yellow",
+  },
+  {
+    business_name: "Corner Burger",
+    longitude: 19.949498706014293,
+    latitude: 50.05072253795349,
+    discount: 40,
+    color: "orange",
+  },
+  {
+    business_name: "Okrąglak",
+    longitude: 19.94513732300949,
+    latitude: 50.052044895190356,
+    discount: 4,
+    color: "gray",
+  },
+  {
+    business_name: "Bistro akademickie KRAKUS",
+    longitude: 19.917133850606643,
+    latitude: 50.065349885938694,
+    discount: 40,
+    color: "gold",
+  },
+  {
+    business_name: "Awokado Lunch Bar",
+    longitude: 19.9360165339074,
+    latitude: 50.07637024018192,
+    discount: 40,
+    color: "magenta",
+  },
+];
+
 export function Map() {
   const mapContainer = useRef(null);
   const map = useRef(null);
@@ -31,11 +90,25 @@ export function Map() {
     });
     map.current.addControl(gc);
 
-    const marker = new maptilersdk.Marker({ color: "#ba1a1a" })
-      .setLngLat([19.93, 50.06])
-      .setPopup(new maptilersdk.Popup().setHTML("Hello World!"))
-      .addTo(map.current);
-  }, [cracow.lng, cracow.lat, zoom]);
+    localizations.forEach(
+      ({ business_name, latitude, longitude, discount, color }) => {
+        new maptilersdk.Marker({ color: color })
+          .setLngLat([longitude, latitude])
+          .setPopup(
+            new maptilersdk.Popup().setHTML(
+              `<div class="markerBody">
+                  <h1 class="markerTop">${business_name}</h1>
+                  <p class="markerBottom">!!! Zniżka studencka: ${discount}% !!!</p>
+                  <p class="markerCord">${latitude}, ${longitude}</p>
+                  
+              </div>
+              `,
+            ),
+          )
+          .addTo(map.current);
+      },
+    );
+  }, [cracow.lng, cracow.lat, zoom, localizations]);
 
   return (
     <div className="map-wrap">
